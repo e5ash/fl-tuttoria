5b844ea0fa5e19394fe6f22c6451dc591b9d284b
e5ash <www@e5ash.com>
2021-11-10 16:33:17 +0400

    80%

--- source/dev/_/sys/js/common.js
+++ source/dev/_/sys/js/common.js
@@ -3,3 +3,46 @@ var html = document.querySelector('html'),
 		wrap = document.querySelector('.wrap');
 
 var mailPattern = /^[0-9a-z_-]+@[0-9a-z_-]+.[a-z]{2,5}$/i;
+
+
+let certs = document.querySelector('.certs__inner');
+if (certs) {
+	let certsSwiper = new Swiper(certs, {
+		loop: true,
+		loopAdditionalSlides: 6,
+		slidesPerView: 'auto',
+		spaceBetween: 20,
+		navigation: {
+			nextEl: '.certs__arrow.swiper-button-next',
+			prevEl: '.certs__arrow.swiper-button-prev',
+		},
+		pagination: {
+			el: '.certs__nav.swiper-pagination',
+			type: 'bullets',
+		},
+
+	});
+}
+
+let inputs = document.querySelectorAll('.input');
+if (inputs) {
+	inputs.forEach((input)=>{
+		input.area = input.querySelector('input');
+		IMask(
+			input.area, {
+	    mask: '+{7} (000) 000-00-00'
+	  });
+	});
+}
+
+let faqs = document.querySelectorAll('.faq__item');
+if (faqs) {
+	faqs.forEach((faq)=>{
+		faq.answer = faq.querySelector('.faq__a');
+		faq.question = faq.querySelector('.faq__q');
+
+		faq.answer.addEventListener('click', ()=>{
+			faq.classList.toggle('--open');
+		});
+	});
+}
